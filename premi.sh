a04db889d149af3e3d90217e672427a820637e74
kiryusekei <149218417+kiryusekei@users.noreply.github.com>
2024-09-29 20:39:18 +0700

    Add files via upload

--- premi.sh
+++ premi.sh
@@ -667,15 +667,12 @@ function ins_backup(){
 clear
 print_install "Memasang Backup Server"
 #BackupOption
-apt install rclone -y
+curl https://rclone.org/install.sh | bash
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "${REPO}limit/rclone.conf"
-#Install Wondershaper
-cd /bin
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/praiman99/AutoScriptVPN-AIO/Beginner/rclone.conf"
 git clone  https://github.com/magnific0/wondershaper.git
 cd wondershaper
-sudo make install
-cd
+make install
 rm -rf wondershaper
 echo > /home/limit
 apt install msmtp-mta ca-certificates bsd-mailx -y
@@ -684,17 +681,18 @@ defaults
 tls on
 tls_starttls on
 tls_trust_file /etc/ssl/certs/ca-certificates.crt
-
 account default
 host smtp.gmail.com
 port 587
 auth on
-user oceantestdigital@gmail.com
-from oceantestdigital@gmail.com
-password jokerman77 
+user revolution.become.true@gmail.com
+from revolution.become.true@gmail.com
+password rmjydsqnwhehcanj  
 logfile ~/.msmtp.log
 EOF
 chown -R www-data:www-data /etc/msmtprc
+cd /usr/bin
+rm -f /root/set-br.sh
 wget -q -O /etc/ipserver "${REPO}limit/ipserver" && bash /etc/ipserver
 print_success "Backup Server"
 }
